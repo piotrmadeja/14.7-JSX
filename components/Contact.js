d70df16e3c0a507e672fba073512e55e48f371ee
piotrmadeja <36596744+piotrmadeja@users.noreply.github.com>
2018-05-14 16:41:12 +0200

    formatting...

--- components/Contact.js
+++ components/Contact.js
@@ -1,18 +1,19 @@
 var Contact = React.createClass({
-	propTypes: {
-		item: React.PropTypes.object.isRequired,
-	},
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
   render: function(){
-		return(
-			<div className={'contactItem'}>
+    return (
+      <div className={'contactItem'}>
         <img className={'contactImage'} src={'doge.jpg'}></img>
-	        <p className={'contactData'}>Imię: {this.props.item.firstName}
+	  <p className={'contactData'}>Imię: {this.props.item.firstName}
           </p>
-	        <p className={'contactData'}>Nazwisko: {this.props.item.lastName}</p>
-	        <p className={'contactEmail'}
-	          href = {'mailto' + this.props.item.email}>Email: {this.props.item.email}
-	        </p>
+	  <p className={'contactData'}>Nazwisko: {this.props.item.lastName}
+	  </p>
+	  <p className={'contactEmail'}
+	    href = {'mailto' + this.props.item.email}>Email: {this.props.item.email}
+	  </p>
       </div>
-		);
-	}
+    );
+  }
 });
